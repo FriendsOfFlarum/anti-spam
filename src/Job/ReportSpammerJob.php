c971c838b4dcbbea93517642eab880350c896f77
Ian Morland <ian@morland.me>
2023-11-15 21:16:08 +0000

    chore: move reporting to sfs to queue job

---
+++ src/Job/ReportSpammerJob.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace FoF\AntiSpam\Job;
+
+use Flarum\Queue\AbstractJob;
+use Flarum\User\User;
+use FoF\AntiSpam\StopForumSpam;
+use Psr\Log\LoggerInterface;
+
+class ReportSpammerJob extends AbstractJob
+{
+    protected $user;
+    
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+    public function handle(StopForumSpam $sfs, LoggerInterface $log)
+    {
+        if (! $sfs->isEnabled()) {
+            return;
+        }
+
+        $post = $this->user->posts()->first();
+
+        $ipAddress = '8.8.8.8';
+
+        if ($post && filter_var($post->ip_address, FILTER_VALIDATE_IP, [FILTER_FLAG_NO_PRIV_RANGE])) {
+            $ipAddress = $post->ip_address;
+        }
+
+        try {
+            $sfs->report([
+                'ip_addr'  => $ipAddress,
+                'username' => $this->user->username,
+                'email'    => $this->user->email,
+            ]);
+        } catch (\Throwable $e) {
+            $log->error("Failed to report spammer to StopForumSpam: {$e->getMessage()}");
+        }
+    }
+}
--- src/Listener/ReportSpammer.php
+++ src/Listener/ReportSpammer.php
@@ -11,53 +11,21 @@
 
 namespace FoF\AntiSpam\Listener;
 
-use Flarum\Foundation\ValidationException;
 use FoF\AntiSpam\Event\MarkedUserAsSpammer;
-use FoF\AntiSpam\StopForumSpam;
-use GuzzleHttp\Exception\RequestException;
-use GuzzleHttp\Psr7\Message;
+use FoF\AntiSpam\Job\ReportSpammerJob;
+use Illuminate\Contracts\Queue\Queue;
 
 class ReportSpammer
 {
-    /**
-     * @var StopForumSpam
-     */
-    private $sfs;
+    protected $queue;
 
-    public function __construct(StopForumSpam $sfs)
+    public function __construct(Queue $queue)
     {
-        $this->sfs = $sfs;
+        $this->queue = $queue;
     }
 
     public function handle(MarkedUserAsSpammer $event)
     {
-        if (! $this->sfs->isEnabled()) {
-            return;
-        }
-
-        $user = $event->user;
-        $post = $user->posts()->first();
-
-        $ipAddress = '8.8.8.8';
-
-        if ($post && filter_var($post->ip_address, FILTER_VALIDATE_IP, [FILTER_FLAG_NO_PRIV_RANGE])) {
-            $ipAddress = $post->ip_address;
-        }
-
-        try {
-            $this->sfs->report([
-                'ip_addr'  => $ipAddress,
-                'username' => $user->username,
-                'email'    => $user->email,
-            ]);
-        } catch (RequestException $e) {
-            throw new ValidationException([
-                'sfs' => strip_tags(Message::bodySummary($e->getResponse())),
-            ]);
-        } catch (\Throwable $e) {
-            throw new ValidationException([
-                'sfs' => resolve('translator')->trans('fof-stopforumspam.api.error.unknown'),
-            ]);
-        }
+        $this->queue->push(new ReportSpammerJob($event->user));
     }
 }
